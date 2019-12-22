ec53c772d459cf1773722d1b261e2bd121fd5096
Yevhenii <yevhenii@Yevheniis-MacBook-Pro.local>
2019-12-22 22:49:44 +0200

    finished task2 in HM#6

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let a = prompt('Enter number for a');
+
+let b = prompt('Enter number for b');
+
+let c = prompt('Enter number for c');
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert('Input values should be ONLY numbers');
+} else if (a == '0' || b == '0' || c == '0') {
+  alert('A triangle must have 3 sides with a positive definite length');
+} else if (a < '0' || b < '0' || c < '0') {
+  console.log('Triangle doesn’t exist');
+  alert('Triangle doesn’t exist');
+} else if (a === b && b === c) {
+  console.log('Equalateral');
+} else if (a === b || b === c || a === c) {
+  console.log('Isosceles');
+} else {
+  console.log('Scalene!');
+}
