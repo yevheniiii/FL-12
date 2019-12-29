97c51105464c4d46b3c572432da8c9eea7461e06
Yevhenii <yevhenii@Yevheniis-MacBook-Pro.local>
2019-12-29 13:59:24 +0200

    refactored task2; make game endless until a user quits or loses; fized user's message

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,102 +1,76 @@
-let start = confirm('Do you want to play a game?');
-let minNumber = 0;
+const MIN_NUMBER = 0;
 let maxNumber = 8;
-const DOUBLE_FIRST_ATTEMPT_PRIZE = 200;
-const FIRST_ATTEMPT_PRIZE = 100;
-const SECOND_ATTEMPT_PRIZE = 50;
-const THIRD_ATTEMPT_PRIZE = 25;
+const MAX_NUMBER_COEF = 4;
+const PRIZE_COEF_INCREASE = 2;
+let FIRST_ATTEMPT_PRIZE = 100;
+let SECOND_ATTEMPT_PRIZE = 50;
+let THIRD_ATTEMPT_PRIZE = 25;
 let prizes = [FIRST_ATTEMPT_PRIZE, SECOND_ATTEMPT_PRIZE, THIRD_ATTEMPT_PRIZE];
 let gameNumberRandom =
-  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  Math.floor(Math.random() * (maxNumber - MIN_NUMBER + 1)) + MIN_NUMBER;
 let userNumber;
 const ATTEMPTS = 3;
-let continueGame;
 let totalPrize = 0;
 let userAttempts = 3;
 
-if (!start) {
+let startGame = confirm('Do you want to play a game?');
+
+if (!startGame) {
   alert('You did not become a billionaire, but can.');
 } else {
   for (let i = 0; i < ATTEMPTS; i++) {
     userNumber = prompt(
-      `Choose a roulette pocket number from 0 to 8
+      `Choose a roulette pocket number from 0 to ${maxNumber}
        Attempts left: ${userAttempts}
        Total prize: ${totalPrize}$
        Possible prize on current attempt: ${prizes[i]}$`
     );
+    if (!userNumber) {
+      break;
+    }
     userAttempts -= 1;
     if (Number(userNumber) === gameNumberRandom) {
-      if (i === 0) {
-        totalPrize = prizes[0];
-
-        continueGame = confirm(
-          `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-        );
-        break;
-      } else if (i === 1) {
-        totalPrize = prizes[SECOND_ATTEMPT_PRIZE];
-
-        continueGame = confirm(
-          `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-        );
+      totalPrize += prizes[i];
+      startGame = confirm(
+        `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
+      );
+      if (!startGame) {
         break;
       } else {
-        totalPrize = prizes[THIRD_ATTEMPT_PRIZE];
-        continueGame = confirm(
-          `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-        );
-        break;
+        i = 0 - 1;
+        maxNumber = maxNumber + MAX_NUMBER_COEF;
+        gameNumberRandom =
+          Math.floor(Math.random() * (maxNumber - MIN_NUMBER + 1)) + MIN_NUMBER;
+        userAttempts = ATTEMPTS;
+        FIRST_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        SECOND_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        THIRD_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        prizes = [
+          FIRST_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE,
+          SECOND_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE,
+          THIRD_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE
+        ];
       }
     }
     if (i === ATTEMPTS - 1 && Number(userNumber) !== gameNumberRandom) {
       alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
-      continueGame = confirm('Do you want to play again?');
-    }
-  }
-  if (continueGame) {
-    const MAX_Number = 12;
-    gameNumberRandom =
-      Math.floor(Math.random() * (MAX_Number - minNumber + 1)) + minNumber;
-    userAttempts = ATTEMPTS;
-    prizes = [
-      DOUBLE_FIRST_ATTEMPT_PRIZE,
-      FIRST_ATTEMPT_PRIZE,
-      SECOND_ATTEMPT_PRIZE
-    ];
-    for (let i = 0; i < ATTEMPTS; i++) {
-      userNumber = prompt(
-        `Choose a roulette pocket number from 0 to 12
-         Attempts left: ${userAttempts}
-         Total prize: ${totalPrize}$
-         Possible prize on current attempt: ${prizes[i]}$`
-      );
-      userAttempts -= 1;
-      if (Number(userNumber) === gameNumberRandom) {
-        if (i === 0) {
-          totalPrize += prizes[0];
-          continueGame = confirm(
-            `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-          );
-          break;
-        } else if (i === 1) {
-          totalPrize += prizes[1];
-          continueGame = confirm(
-            `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-          );
-          break;
-        } else {
-          totalPrize += prizes[THIRD_ATTEMPT_PRIZE];
-          continueGame = confirm(
-            `Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`
-          );
-          break;
-        }
-      }
-      if (i === ATTEMPTS - 1 && Number(userNumber) !== gameNumberRandom) {
-        alert(
-          `Thank you for your participation. Your prize is: ${totalPrize}$`
-        );
-        continueGame = confirm('Do you want to play again?');
+      startGame = confirm('Do you want to play again?');
+      if (!startGame) {
+        break;
+      } else {
+        i = 0 - 1;
+        maxNumber = maxNumber + MAX_NUMBER_COEF;
+        gameNumberRandom =
+          Math.floor(Math.random() * (maxNumber - MIN_NUMBER + 1)) + MIN_NUMBER;
+        userAttempts = ATTEMPTS;
+        FIRST_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        SECOND_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        THIRD_ATTEMPT_PRIZE *= PRIZE_COEF_INCREASE;
+        prizes = [
+          FIRST_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE,
+          SECOND_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE,
+          THIRD_ATTEMPT_PRIZE * PRIZE_COEF_INCREASE
+        ];
       }
     }
   }
