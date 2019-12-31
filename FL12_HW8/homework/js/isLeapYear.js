d317a109f57b8fb85aec941ef91890d37098e403
Yevhenii <yevhenii@Yevheniis-MacBook-Pro.local>
2019-12-31 13:54:47 +0200

    finished isLeapYear task

---
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -0,0 +1,13 @@
+let isLeap = year => {
+  let finalResult = new Date(year).getFullYear();
+
+  if (isNaN(finalResult)) {
+    return 'Invalid Date';
+  } else {
+    return (finalResult % 4 === 0 && finalResult % 100 !== 0) ||
+      finalResult % 400 === 0
+      ? `${finalResult} is a leap year`
+      : `${finalResult} is not a leap year`;
+  }
+};
+console.log(isLeap('2026-01-15 13:00:00'));
