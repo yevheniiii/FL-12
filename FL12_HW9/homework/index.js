493b6fc0c82fbaa51190f5aef461e3426a9d4f77
Yevhenii <yevhenii@Yevheniis-MacBook-Pro.local>
2020-01-11 19:55:42 +0200

    finished 2 and 3 task

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -12,3 +12,22 @@ const convert = (...args) => {
 
 convert('1', '2', '3', '4');
 
+let executeforEach = (arr, func) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(func(arr[i]));
+  }
+  return result;
+};
+
+executeforEach([1, 2, 3], function(el) {
+  console.log(el * 2);
+});
+
+let mapArray = (arr, func) => {
+  console.log(executeforEach(arr, func));
+};
+
+mapArray([2, '5', 8], function(el) {
+  return Number(el) + 3;
+});
