de6acf13e0730032809cd94841210a824a9df932
Yevhenii <yevhenii@Yevheniis-MacBook-Pro.local>
2020-01-26 16:45:50 +0200

    started HW11

---
+++ FL12_HW11/homework/app.js
@@ -0,0 +1,44 @@
+const structure = [
+    {
+      'folder': true,
+      'title': 'Films',
+      'children': [
+        {
+          'title': 'Iron Man.avi'
+        },
+        {
+          'folder': true,
+          'title': 'Fantasy',
+          'children': [
+            {
+              'title': 'The Lord of the Rings.avi'
+            },
+            {
+              'folder': true,
+              'title': 'New folder 1',
+              'children': false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      'folder': true,
+      'title': 'Documents',
+      'children': [
+        {
+          'folder': true,
+          'title': 'EPAM Homework answers',
+          'children': null
+        }
+      ]
+    }
+];
+
+const rootNode = document.getElementById('root');
+
+// Todo: your code goes here
+// const newDiv = document.createElement('div');
+// // newDiv.className = 'first-folder';
+// newDiv.innerHTML = '<ul><li><i class="material-icons">folder</i><span>Films</span></li></ul>';
+// rootNode.appendChild(newDiv);
